50aa5fe53d0dda917f71e45236aef17ab0165d4d
Marc Dickenson <marcdickenson@gmail.com>
2012-06-25 13:42:17 -0700

    fixed bug with new file names in psv dir

--- src/com/github/stocker/abstraction/StockerGlobalDataPsv.java
+++ src/com/github/stocker/abstraction/StockerGlobalDataPsv.java
@@ -4,11 +4,14 @@
 package com.github.stocker.abstraction;
 
 import java.io.File;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
 
 /**
  * Implementation of StockerInfoData class.
@@ -25,17 +28,14 @@ public class StockerGlobalDataPsv extends StockerGlobalData {
         ArrayList<String> l_data = new ArrayList<String>();
         File l_dir = new File("resources/stock_data/psv/");
         try {
-            String[] l_fileExts = {"psv"};
-
-            Collection<File> l_files = FileUtils.listFiles(l_dir, l_fileExts, false);
- 
+            Collection<File> l_files = FileUtils.listFiles(l_dir, new PrefixFileFilter("data"), new SuffixFileFilter("psv")); 
             for (Iterator<File> iterator = l_files.iterator(); iterator.hasNext();) {
                 File l_tmpFile = (File) iterator.next();
                 String[] l_tokens = l_tmpFile.getName().split("[.]");
                 // data.<symbol>.*
-                //System.out.println(l_tokens[0] + l_tokens[1] + l_tokens[2]);
-                if(!l_data.contains(l_tokens[2])) {
-                    l_data.add(l_tokens[2]);
+                System.out.println(l_tokens[0] + l_tokens[1] + l_tokens[2]);
+                if(!l_data.contains(l_tokens[1])) {
+                    l_data.add(l_tokens[1]);
                 }
             }
         } catch (Exception e) {
